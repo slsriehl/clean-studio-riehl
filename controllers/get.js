f11357f611e62dcf06e6c74d43f8a29a387ca250
Sarah Riehl <sls.riehl@gmail.com>
2017-09-14 20:40:28 -0500

    strip routes in prep for single page

--- controllers/get.js
+++ controllers/get.js
@@ -10,32 +10,34 @@ const controller = {
 		const footerYear = helpers.footerYear();
 	  res.render('index.hbs', {
 			footerYear: footerYear,
-			brandSelect: true
+			fullStackSites: fullStackData,
+			wordpressSites: wordpressData
+			// brandSelect: true
 		});
 	},
-	renderMail: (req, res) => {
-		const footerYear = helpers.footerYear();
-		res.render('mail.hbs', {
-			footerYear: footerYear,
-			mailSelect: true
-		});
-	},
-	renderWordpress: (req, res) => {
-		const footerYear = helpers.footerYear();
-		res.render('wordpress.hbs', {
-			sites: wordpressData,
-			footerYear: footerYear,
-			wordpressSelect: true
-		});
-	},
-	renderFullStack: (req, res) => {
-		const footerYear = helpers.footerYear();
-		res.render('full-stack.hbs', {
-			sites: fullStackData,
-			footerYear: footerYear,
-			fullStackSelect: true
-		});
-	},
+	// renderMail: (req, res) => {
+	// 	const footerYear = helpers.footerYear();
+	// 	res.render('mail.hbs', {
+	// 		footerYear: footerYear,
+	// 		mailSelect: true
+	// 	});
+	// },
+	// renderWordpress: (req, res) => {
+	// 	const footerYear = helpers.footerYear();
+	// 	res.render('wordpress.hbs', {
+	// 		sites: wordpressData,
+	// 		footerYear: footerYear,
+	// 		wordpressSelect: true
+	// 	});
+	// },
+	// renderFullStack: (req, res) => {
+	// 	const footerYear = helpers.footerYear();
+	// 	res.render('full-stack.hbs', {
+	// 		sites: fullStackData,
+	// 		footerYear: footerYear,
+	// 		fullStackSelect: true
+	// 	});
+	// },
 
 }
 
