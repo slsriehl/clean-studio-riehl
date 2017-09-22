c87ce86d803c4f1c7ec60ab64edb892bfa5617a0
Sarah Riehl <sls.riehl@gmail.com>
2017-09-22 17:27:10 -0500

    edit out more links until further development

--- data/full-stack.js
+++ data/full-stack.js
@@ -8,7 +8,7 @@ module.exports = [{
 	imgPath: `${imgPath}hematogones.jpg`,
 	technologies: "nodejs, mysql, handlebars, bootstrap",
 	beta: true,
-	more: "All-star pathologist Chandra Krishnan created this popular site to generate lab reports based on raw test data.  After several years, he hired Studio Riehl to create a Nodejs login-based back-end that saves, PDFs, and emails reports, shows previously created reports, and incorporates case references.  We worked closely with the client to integrate the front end and the back end and set him up to continue to expand the site on his own."
+	//more: "All-star pathologist Chandra Krishnan created this popular site to generate lab reports based on raw test data.  After several years, he hired Studio Riehl to create a Nodejs login-based back-end that saves, PDFs, and emails reports, shows previously created reports, and incorporates case references.  We worked closely with the client to integrate the front end and the back end and set him up to continue to expand the site on his own."
 }, {
 	siteName: "JonRiehl.com",
 	strictUrl: "https://jonriehl.com",
@@ -17,7 +17,7 @@ module.exports = [{
 	imgPath: `${imgPath}jon-riehl.jpg`,
 	technologies: "php, custom layout",
 	beta: false,
-	more: "This veteran Python developer had his heart set on a portfolio site that looked like a Commodore 64 and could be hosted in PHP.  We wrote custom Sass to accomplish his goals."
+	//more: "This veteran Python developer had his heart set on a portfolio site that looked like a Commodore 64 and could be hosted in PHP.  We wrote custom Sass to accomplish his goals."
 }, {
 	siteName: "Simply Wild Gardens",
 	strictUrl: "https://simplywildgardens.com",
@@ -26,7 +26,7 @@ module.exports = [{
 	imgPath: `${imgPath}simply-wild.jpg`,
 	technologies: "php, custom layout",
 	beta: false,
-	more: "Organic gardening maven Suzi Sands has a background in graphic design and knew exactly what she wanted her site to look like.  We wrote custom Sass to express her vision and added a mobile layout."
+	//more: "Organic gardening maven Suzi Sands has a background in graphic design and knew exactly what she wanted her site to look like.  We wrote custom Sass to express her vision and added a mobile layout."
 }, {
 	siteName: "Legacy Electrical Services",
 	strictUrl: "https://intense-oasis-58048.herokuapp.com/",
@@ -35,5 +35,5 @@ module.exports = [{
 	imgPath: `${imgPath}legacy.jpg`,
 	technologies: "nodejs, bootstrap, yelp.com API",
 	beta: true,
-	more: ""
+	//more: ""
 }];
