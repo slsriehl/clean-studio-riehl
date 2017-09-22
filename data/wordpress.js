1765fbeb11c1d63ad691cc6dc9133b313ae06592
Sarah Riehl <sls.riehl@gmail.com>
2017-09-22 15:07:32 -0500

    data: add site info

---
+++ data/wordpress.js
@@ -0,0 +1,27 @@
+const imgPath = "img/sites/wordpress/";
+
+module.exports = [{
+	siteName: "Wildflower Church",
+	strictUrl: "https://wildflowerchurch.org",
+	friendlyUrl: "wildflowerchurch.org",
+	siteYear: "2016",
+	imgPath: `${imgPath}wfc.jpg`
+}, {
+	siteName: "Schieffer Studio",
+	strictUrl: "https://schiefferstudio.com",
+	friendlyUrl: "schiefferstudio.com",
+	siteYear: "2015",
+	imgPath: `${imgPath}schieffer-studio.jpg`
+}, {
+	siteName: "Mobile Vet Austin",
+	strictUrl: "https://mobilevetaustin.com",
+	friendlyUrl: "mobilevetaustin.com",
+	siteYear: "2016",
+	imgPath: `${imgPath}mobile-vet-austin.jpg`
+}, {
+	siteName: "Salon 202",
+	strictUrl: "https://salon202austin.com",
+	friendlyUrl: "salon202austin.com",
+	siteYear: "2017",
+	imgPath: `${imgPath}salon202.jpg`
+}];
