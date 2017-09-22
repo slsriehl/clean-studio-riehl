49e6919837de48892698231af518ea8c9586f312
Sarah Riehl <sls.riehl@gmail.com>
2017-09-22 15:08:04 -0500

    routes add post route for mail send

--- routes/post.js
+++ routes/post.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const router = new express.Router;
 
+const controller = require('../controllers/post');
+
+router.post('/mail', (req, res) => {
+	controller.dispatchMail(req, res);
+});
 
 
 module.exports = router;
