17a1274870eae644fdc70a82bcdeb7dce4ef2eca
Sarah Riehl <sls.riehl@gmail.com>
2017-09-22 20:17:45 -0500

    compiled/concatted js and css

--- public/js/index.js
+++ public/js/index.js
@@ -23,28 +23,52 @@
  */
 //adapted from https://codepen.io/zchee/pen/ogzvZZ
 
-(function() {
-	'use strict';
 
+//++++++ bind and unbind the window scroll with this function ++++++
+var onScrollFunction = function(event) {
+	//empty object to push section ids to
 	var sections = {};
-
+	//pick out the sections of the site that correspond to menu items
 	var sectionSites = [].slice.call(document.getElementsByClassName("section__sites"));
+	//loop the sections of the site
 	for(var i = 0; i < sectionSites.length; i++) {
+		// in the sections object, create a sub obj for each section and give it an id pair and an offsetTop pair
+		console.log(sectionSites[i].id + ' -- ' + sectionSites[i].offsetTop);
 		sections[sectionSites[i].id] = sectionSites[i].offsetTop;
 	}
+	//the scroll position is where you are on the page vertically
+	var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
 
-	window.onscroll = function() {
-		var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
-
-		var i = 0;
-		for (i in sections) {
-			if (sections[i] <= (scrollPosition + 40)) {
-				document.querySelector('.header--inverse').classList.remove('header--inverse');
-				document.querySelector('a[href*=' + i + ']').classList.add('header--inverse');
-			}
+	var i = 0;
+	//loop the sections object
+	for (i in sections) {
+		//if you're past the section, remove the highlight class from the old section and add the highlight class to the next section
+		if (sections[i] <= scrollPosition) {
+			document.querySelector('.header--inverse').classList.remove('header--inverse');
+			document.querySelector('a[href*="' + i + '"]').classList.add('header--inverse');
 		}
-	};
-})();
+	}
+	window.location.hash = document.querySelector('.header--inverse').getAttribute('href');
+}
+
+var removeHighlighting = function() {
+	if(document.querySelector('.header--inverse')) {
+		document.querySelector('.header--inverse').classList.remove('header--inverse');
+	}
+}
+
+var addHighlighting = function() {
+	if(window.location.hash) {
+		document.querySelector('a[href*="' + window.location.hash + '"]').classList.add('header--inverse');
+	} else {
+		document.querySelector('a[href*="#about"]').classList.add('header--inverse');
+	}
+
+
+}
+
+
+//++++++ use these functions to convert the form input to json so the xhr request can send it ++++++
 
 //validate the fields sent to form to JSON so they're excluded if blank
 var isValidElement = function(element) {
@@ -66,35 +90,47 @@ var formToJson = function(elements) {
 	}, {});
 }
 
-//xml http request class and make request promise method
+//++++++ xml http request class and make request promise method ++++++
+
+//pass the method, path, and object to be sent
 var request = function(method, path, data) {
+	//create a promise
 	return new Promise(function(resolve, reject) {
+		//create an xhr request
 		var req = new XMLHttpRequest();
-		//req.setRequestHeader "Content-Type", "application/json;charset=UTF-8"
+		//open the connection
 		req.open(method, path);
+		//tell the server to expect a json string
 		req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+		//once you get the response back
 		req.onload = function() {
+			//if the data reached the server, fulfill the promise with the string returned from the server
 			if(req.status == 200) {
 				resolve(req.responseText);
 			} else {
+				//otherwise the data didn't reach the server so reject the promise with an error
 				reject(new Error(req.statusText));
 			}
 		}
+		//if there's a different error, reject the promise with an error
 		req.onerror = function() {
 			reject(new Error('unknown error'));
 		}
+		//return the result of the promise based on sending the json string of data to the server
 		return req.send(JSON.stringify(data));
 	});
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+	//+++++ mobile menu toggle section +++++
+
 	//pick out most elements for menu toggle
 	var menuToggle = document.getElementById('menuToggle');
 	var headerMenu = document.getElementById('headerMenu');
 	var menuOverlay = document.getElementById('menuOverlay');
 
-	//define menu toggle function
-	var toggleMenu = function() {
+	//define menu toggle function to call onclick of the toggle, the overlay, or a menu item
+	var toggleMenu = function(event) {
 		if(window.getComputedStyle(headerMenu, null).getPropertyValue('visibility') == 'hidden') {
 			//console.log('toggle menu');
 			headerMenu.style.visibility = 'visible';
@@ -106,50 +142,100 @@ document.addEventListener('DOMContentLoaded', function() {
 			//console.log('toggle no menu');
 			headerMenu.style.visibility = 'hidden';
 			menuOverlay.style.visibility = 'hidden';
-			menuToggle.style.visibility = 'unset';
+			menuToggle.style.visibility = 'visible';
 			headerMenu.style.zIndex = '-1';
 			menuOverlay.style.zIndex = '-1';
 		}
 	}
 	//toggle menu on hamburger click
-	menuToggle.addEventListener('click', function(event) {
-		toggleMenu();
-	});
+	menuToggle.addEventListener('click', toggleMenu);
 	//toggle menu on overlay click
-	menuOverlay.addEventListener('click', function(event) {
-		toggleMenu();
-	});
+	menuOverlay.addEventListener('click', toggleMenu);
 	//toggle menu on menu item click
 	//pick out menu items by class
 	var menuItems = [].slice.call(document.getElementsByClassName('header__menu__item'));
+	//if the document loads in mobile mode, attach event listeners to menuItems
+	if(document.documentElement.clientWidth < 826) {
+		removeHighlighting();
 	//loop to add event listeners
-	for(var i = 0; i < menuItems.length; i++) {
-		menuItems[i].addEventListener('click', function(event) {
-			toggleMenu();
-		});
+		for(var i = 0; i < menuItems.length; i++) {
+			//add the event listener to toggle the menu closed on item click
+			menuItems[i].addEventListener('click', toggleMenu);
+		}
+	} else {
+		//if the document loads in desktop mode, bind to scroll
+		window.addEventListener('scroll', onScrollFunction);
 	}
 
+	//++++++ on resize, restore proper arrangement for mobile or desktop ++++++
+	//if the window size changes
+	window.addEventListener('resize', function(event) {
+		// if the window is desktop
+		if(document.documentElement.clientWidth >= 826) {
+			//bind to scroll and highlight current section
+			addHighlighting();
+			window.addEventListener('scroll', onScrollFunction);
+			//show the headerMenu, hide the overlay and the toggle button, and raise the z-index of the header menu back up
+			headerMenu.style.visibility = 'visible';
+			menuOverlay.style.visibility = 'hidden';
+			menuToggle.style.display = 'none';
+			headerMenu.style.zIndex = '150';
+			menuOverlay.style.zIndex = '-1';
+			//loop to remove event listeners
+			for(var i = 0; i < menuItems.length; i++) {
+				//remove the event listeners that toggle the menu from the menu items
+				menuItems[i].removeEventListener('click', toggleMenu);
+			}
+		} else {
+		//if it's a mobile screen
+			//remove scroll highlighting
+			removeHighlighting();
+			//unbind from scroll
+			window.removeEventListener('scroll', onScrollFunction);
+			//hide the menu & overlay, show the toggle
+			headerMenu.style.visibility = 'hidden';
+			menuOverlay.style.visibility = 'hidden';
+			menuToggle.style.display = 'block';
+			headerMenu.style.zIndex = '-1';
+			menuOverlay.style.zIndex = '-1';
+			//loop to add event listeners
+			for(var i = 0; i < menuItems.length; i++) {
+				//add the event listener to toggle the menu off on click of a menu item
+				menuItems[i].addEventListener('click', toggleMenu);
+			}
+		}
+	});
 
-//add classes to inputs to enable floating labels
+//++++++ email form ++++++
+
+//++++++ add classes to inputs to enable floating labels ++++++
+	//pick out the input fields
 	var inputs = [].slice.call(document.getElementsByClassName('contact__input'));
 	//console.log(inputs);
+	//loop inputs to add event listener
 	for(var i = 0; i < inputs.length; i++) {
+
 		inputs[i].addEventListener('blur', function() {
 			if(this.value) {
+				//when you move out of an input field, if the input has value, apply the class that retains the focus formatting to keep the label floating
 				this.classList.add('contact__input--filled');
 			} else if(this.classList.contains('contact__input--filled')) {
+				//if the input doesn't have a value, restore the non-floating look of an empty input
 				this.classList.remove('contact__input--filled');
 			}
 		});
 	}
 
+	//++++++ email form submit ++++++
+	//pick out elements to manipulate
 	var emailForm = document.getElementById('emailStudioRiehl');
 	var replaceMe = document.getElementById('replaceOnSubmit');
 
+	//add event listener to call xhr req on form submit
 	emailForm.addEventListener('submit', function(event) {
 		submitForm(event, emailForm, replaceMe);
 	});
-
+	//++++++ end DOMContentLoaded listener ++++++
 });
 
 //function to call on submit form
@@ -193,12 +279,3 @@ var resetJump = function() {
 	//console.log('reset jump');
 	history.pushState ("", document.title, (window.location.pathname + window.location.search));
 }
-
-// //menu item onclick event callback
-//
-// var goToMenuItems = function(parentEl, overlay, toggle, jumpName) {
-// 	//console.log('go to menu items');
-// 	resetJump();
-// 	//toggleMenu parentEl, overlay, toggle
-// 	window.location.hash = "\u0023" + jumpName;
-// }
