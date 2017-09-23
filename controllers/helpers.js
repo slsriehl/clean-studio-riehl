4931fa00f712a218758bb8978d9fbf6c3da085b4
Sarah Riehl <sls.riehl@gmail.com>
2017-09-23 16:26:45 -0500

    single copyright date

--- controllers/helpers.js
+++ controllers/helpers.js
@@ -3,12 +3,7 @@
 const helpers = {
 	footerYear: () => {
 		const thisDate = new Date().getFullYear();
-		console.log(thisDate);
-		if(thisDate == 2017) {
-			return thisDate;
-		} else {
-			return `2017-${thisDate}`;
-		}
+		return `${thisDate}`;
 	}
 }
 
