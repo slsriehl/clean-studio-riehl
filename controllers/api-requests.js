6a7dccc84053e9e1f4275e5f5bc89af3f6c680f2
Sarah Riehl <sls.riehl@gmail.com>
2017-10-12 20:38:30 -0500

    api request work, pdf almost working

--- controllers/api-requests.js
+++ controllers/api-requests.js
@@ -4,6 +4,8 @@ const reqProm = require('request-promise');
 
 const Promise = require('bluebird');
 
+const http = require('http');
+
 const zohoKey = process.env.ZOHO_AUTH_TOKEN;
 
 const zohoId = process.env.ZOHO_ORG_ID;
@@ -20,17 +22,33 @@ const helpers = {
 			}
 		})
 	},
-	getInvoicePdf: (invoice) => {
-		return axios.get(`https://books.zoho.com/api/v3/invoices/${invoice}`, {
+	getInvoicePdf: (res, invoice) => {
+		let data = [];
+		let newData;
+		let options = {
+			method: 'GET',
+			hostname: 'books.zoho.com',
+			path: `/api/v3/invoices/${invoice}?organization_id=${zohoId}&accept=pdf`,
 			headers: {
 				"Authorization" : `Zoho-authtoken ${zohoKey}`,
 				"Content-Type": "application/json;charset=UTF-8"
-			},
-			params: {
-				organization_id: zohoId,
-				accept: "pdf"
 			}
+		}
+		const request = http.request(options, (response) => {
+			console.log(response);
+			response.on('data', (chunk) => {
+				data.push(chunk);
+			});
+
+			response.on('end', () => {
+				console.log(data);
+				data = Buffer.concat(data);
+				res.setHeader('Content-Length', data.length);
+				res.end(data);
+			});
 		})
+
+		request.end();
 	},
 	getCustomer: (contactId) => {
 		return axios.get(`https://books.zoho.com/api/v3/contacts/${contactId}`, {
