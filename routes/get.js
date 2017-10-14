f4e0091ba951c097bc34915f5df453af6f9a60bf
Sarah Riehl <sls.riehl@gmail.com>
2017-10-13 19:47:15 -0500

    add min to scripts for production

--- routes/get.js
+++ routes/get.js
@@ -8,14 +8,14 @@ const controller = require('../controllers/get');
 router.get('/', (req, res) => {
 	controller.renderAbout(req, res, [
 		"https://www.google.com/recaptcha/api.js",
-		"/js/index.js"
+		"/js/index.min.js"
 	]);
 });
 
 router.get('/pay/:invoice', (req, res) => {
 	controller.renderPayment(req, res, true, [
 		"https://js.stripe.com/v3/",
-		"/js/pay.js"
+		"/js/pay.min.js"
 	]);
 });
 
